c7e62665704ae7695b8a96757c0533419a72e213
Maurice Hunter <48691393+MauriceHunter@users.noreply.github.com>
2022-06-28 17:33:05 -0400

    Initial Commit

---
+++ F1Legacy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  F1Legacy
+//
+//  Created by Maurice Hunter on 6/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ F1Legacy/F1LegacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1LegacyApp.swift
+//  F1Legacy
+//
+//  Created by Maurice Hunter on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct F1LegacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
